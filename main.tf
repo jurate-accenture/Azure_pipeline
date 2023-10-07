cf756180ebe03005faf4e392078826b993f8ae71
jurate-accenture <jurate.zizyte@accenture.com>
2023-10-07 22:34:16 +0200

    sdf

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "Terraform_pipelines"
+    storage_account_name  = "terraformjz177"
+    container_name        = "files"
+    key                   = "terraform.tfstate"
+  }
 }
