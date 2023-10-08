c48902ffd49d5db9bf64430ef45791a9b569180e
jurate-accenture <jurate.zizyte@accenture.com>
2023-10-08 21:27:06 +0200

    asdfasf

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 # Define Terraform provider
 terraform {
   required_version = ">= 1.3"
+
   backend "azurerm" {
     resource_group_name  = "Terraform_pipelines"
     storage_account_name = "terraformjz177"
     container_name       = "files"
     key                  = "actions.tfstate"
   }
+
   required_providers {
     azurerm = {
       version = "~>3.2"
@@ -14,12 +16,10 @@ terraform {
     }
   }
 }
+
 # Configure the Azure provider
 provider "azurerm" {
   features = {
     virtual_network_peering = {}
   }
 }
-
- } }
-
