95d4fbf8ffdb792755646f0f9917677ae7c0dbb7
jurate-accenture <jurate.zizyte@accenture.com>
2023-10-08 22:18:46 +0200

    askdjf

--- az-remote-backend-main.tf
+++ az-remote-backend-main.tf
@@ -1,27 +1,16 @@
 # Generate a random storage name
 resource "random_string" "tf-name" {
-  length = 8
-  upper = false
+  length  = 15  # Limit the length directly to 15 characters
+  upper   = false
   numeric = true
-  lower = true
+  lower   = true
   special = false
 }
-# Create a Resource Group for the Terraform State File
-resource "azurerm_resource_group" "state-rg" {
-  name = "${lower(var.company)}-tfstate-rg"
-  location = var.location
-  
-  lifecycle {
-    prevent_destroy = true
-  }  
-  tags = {
-    environment = var.environment
-  }
-}
+
 # Create a Storage Account for the Terraform State File
 resource "azurerm_storage_account" "state-sta" {
   depends_on               = [azurerm_resource_group.state-rg]
-  name = "${lower(replace(var.company, "[^a-z0-9]", ""))}tfr${substr(random_string.tf-name.result, 0, 15)}"
+  name = "${lower(replace(var.company, "[^a-z0-9]", ""))}tfr${random_string.tf-name.result}"
   resource_group_name      = azurerm_resource_group.state-rg.name
   location                 = azurerm_resource_group.state-rg.location
   account_kind             = "StorageV2"
@@ -37,16 +26,4 @@ resource "azurerm_storage_account" "state-sta" {
   tags = {
     environment = var.environment
   }
-}
-
-output "random_string" {
-  value = random_string.tf-name.result
-}
-
-
-# Create a Storage Container for the Core State File
-resource "azurerm_storage_container" "core-container" {
-  depends_on = [azurerm_storage_account.state-sta]  
-  name = "core-tfstate"
-  storage_account_name = azurerm_storage_account.state-sta.name
 }
