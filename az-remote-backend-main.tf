e25e91f370f699ac9e50c74595d1e25196d65dc5
jurate-accenture <jurate.zizyte@accenture.com>
2023-10-08 22:04:58 +0200

    askdjf

--- az-remote-backend-main.tf
+++ az-remote-backend-main.tf
@@ -20,24 +20,25 @@ resource "azurerm_resource_group" "state-rg" {
 }
 # Create a Storage Account for the Terraform State File
 resource "azurerm_storage_account" "state-sta" {
-  depends_on = [azurerm_resource_group.state-rg]  
-  name = "${lower(var.company)}tf${substr(random_string.tf-name.result, 0, 21)}"
-  resource_group_name = azurerm_resource_group.state-rg.name
-  location = azurerm_resource_group.state-rg.location
-  account_kind = "StorageV2"
-  account_tier = "Standard"
-  access_tier = "Hot"
+  depends_on               = [azurerm_resource_group.state-rg]
+  name                     = "${lower(replace(var.company, "[^a-z0-9]", ""))}tf${substr(random_string.tf-name.result, 0, 21)}"
+  resource_group_name      = azurerm_resource_group.state-rg.name
+  location                 = azurerm_resource_group.state-rg.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  access_tier              = "Hot"
   account_replication_type = "ZRS"
   enable_https_traffic_only = true
-   
+
   lifecycle {
     prevent_destroy = true
-  }  
-  
+  }
+
   tags = {
     environment = var.environment
   }
 }
+
 # Create a Storage Container for the Core State File
 resource "azurerm_storage_container" "core-container" {
   depends_on = [azurerm_storage_account.state-sta]  
