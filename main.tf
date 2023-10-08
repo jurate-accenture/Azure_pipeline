eeab06934921f4c2e5fc55c3c8e3cbe4eec5007c
jurate-accenture <jurate.zizyte@accenture.com>
2023-10-08 20:57:58 +0200

    asdfsadf

--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
+# Define Terraform provider
 terraform {
+  required_version = ">= 1.3"
   backend "azurerm" {
-    resource_group_name   = "Terraform_pipelines"
-    storage_account_name  = "terraformjz177"
-    container_name        = "files"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "kopicloud-tfstate-rg"
+    storage_account_name = "kopicloudiactest"
+    container_name       = "core-tfstate"
+    key                  = "actions.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      version = "~>3.2"
+      source  = "hashicorp/azurerm"
+    }
   }
 }
+# Configure the Azure provider
+provider "azurerm" { 
+
+ features = {
+    # Enable the Virtual Network Peering feature
+    virtual_network_peering { }
+    
+    # Enable the Managed Disks feature
+    managed_disks { }
+    
+    # Enable the Advanced Networking features
+    advanced_networking { }
+  }
+
+
+  }
+}
+
