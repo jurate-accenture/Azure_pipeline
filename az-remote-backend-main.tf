67c59777d0af526e66e44b8be10ab93f6e5acb86
jurate-accenture <jurate.zizyte@accenture.com>
2023-10-09 09:55:57 +0200

    askdjf

--- az-remote-backend-main.tf
+++ az-remote-backend-main.tf
@@ -1,14 +1,8 @@
-# Generate a random storage name
-resource "random_string" "tf-name" {
-  length = 8
-  upper = false
-  numeric = true
-  lower = true
-  special = false
-}
+
+
 # Create a Resource Group for the Terraform State File
-resource "azurerm_resource_group" "state-rg" {
-  name = "${lower(var.company)}-tfstate-rg"
+resource "azurerm_resource_group" "Terraform_pipelines" {
+  name = "Terraform_pipelines"
   location = var.location
   
   lifecycle {
@@ -18,21 +12,14 @@ resource "azurerm_resource_group" "state-rg" {
     environment = var.environment
   }
 }
-# Generate a random storage name
-resource "random_string" "tf-name" {
-  length  = 15  # Limit the length directly to 15 characters
-  upper   = false
-  numeric = true
-  lower   = true
-  special = false
-}
+
 
 # Create a Storage Account for the Terraform State File
-resource "azurerm_storage_account" "state-sta" {
-  depends_on               = [azurerm_resource_group.state-rg]
-  name = "${lower(replace(var.company, "[^a-z0-9]", ""))}tfr${random_string.tf-name.result}"
-  resource_group_name      = azurerm_resource_group.state-rg.name
-  location                 = azurerm_resource_group.state-rg.location
+resource "azurerm_storage_account" "terraformjz177" {
+  depends_on               = [azurerm_resource_group.Terraform_pipelines]
+  name = "terraformjz177"
+  resource_group_name      = azurerm_resource_group.Terraform_pipelines.name
+  location                 = azurerm_resource_group.Terraform_pipelines.location
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   access_tier              = "Hot"
@@ -49,14 +36,11 @@ resource "azurerm_storage_account" "state-sta" {
 }
 
 
-output "random_string" {
-  value = random_string.tf-name.result
-}
 
 
 # Create a Storage Container for the Core State File
-resource "azurerm_storage_container" "core-container" {
-  depends_on = [azurerm_storage_account.state-sta]  
-  name = "core-tfstate"
-  storage_account_name = azurerm_storage_account.state-sta.name
+resource "azurerm_storage_container" "files" {
+  depends_on = [azurerm_storage_account.terraformjz177]  
+  name = "files"
+  storage_account_name = azurerm_storage_account.terraformjz177.name
 }
--- az-remote-backend-output.tf
+++ az-remote-backend-output.tf
@@ -1,9 +1,9 @@
 output "terraform_state_resource_group_name" {
-  value = azurerm_resource_group.state-rg.name
+  value = azurerm_resource_group.Terraform_pipelines.name
 }
 output "terraform_state_storage_account" {
-  value = azurerm_storage_account.state-sta.name
+  value = azurerm_storage_account.terraformjz177.name
 }
 output "terraform_state_storage_container_core" {
-  value = azurerm_storage_container.core-container.name
+  value = azurerm_storage_container.files.name
 }
